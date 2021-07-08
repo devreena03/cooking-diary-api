9196896d7831e9e450265152c3297a196d8a449f
devreena03 <rkreena07@gmail.com>
2021-07-08 22:46:17 +0530

    first commit - code setp

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+//load config file
+dotenv.config({ path: "./config/config.env" });
+
+//loads local files
+const connectDb = require("./config/db");
+const app = express();
+
+//connect db
+connectDb();
+
+//body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${port}!`.yellow
+      .bold
+  );
+});
